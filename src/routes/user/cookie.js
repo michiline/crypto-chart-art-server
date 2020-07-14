6bdfd40dd728c6b01bd019dba04f3001a8048668
equidistant <mich.random@pm.me>
2020-07-14 19:29:20 +0200

    test auth route

--- src/routes/user/cookie.js
+++ src/routes/user/cookie.js
@@ -2,11 +2,11 @@ export const set = async (req, res, next) => {
 	try {
 		const cookieOptions = {
 			httpOnly: true,
-			secure: true,
+			// secure: true,
 			// maxAge: 0,
 			// maxAge: constants.response.COOKIE_MAX_AGE, // ttl in ms (remove this option and cookie will die when browser is closed),
 			signed: true,
-			// sameSite: 'strict',
+			sameSite: 'strict',
 			path: '/'
 		}
 		const { sessionId } = req.user
